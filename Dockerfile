23a400941b80a50c4c8b6491e09b67d4653b8c10
Piyush Singh <b221037@iiit-bh.ac.in>
2025-05-13 18:47:43 +0530

    more version changes

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,11 @@
-# Build stage
-FROM mcr.microsoft.com/dotnet/sdk:9.0 AS build
+# Stage 1: Build
+FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build
 WORKDIR /app
-
 COPY . ./
 RUN dotnet publish -c Release -o out
 
-# Runtime stage
-FROM mcr.microsoft.com/dotnet/aspnet:9.0
+# Stage 2: Runtime
+FROM mcr.microsoft.com/dotnet/aspnet:8.0 AS runtime
 WORKDIR /app
-COPY --from=build /app/out .
-
+COPY --from=build /app/out ./
 ENTRYPOINT ["dotnet", "Backend.dll"]
--- obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e60780f8d9dfbcefb19c3f9b69d3192dda8ba518")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab3de42a8884751b46c3c9a54fe3c10c359785bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
